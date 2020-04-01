29ead9100b09fe93ae8a6b4a7470d0b95d824a09
Utku Tarhan <utku.tarhan@hotmail.com>
2020-03-31 21:40:51 -0700

    v.1.0.0
    + Included Apple Business Conduct for iOS App.
    + Replaced the button on the CreditsViewController to Label with the version information.
    + Added missing Copyright information for the .swift files.

--- Catalyst/FrontVideoCapture.swift
+++ Catalyst/FrontVideoCapture.swift
@@ -1,3 +1,15 @@
+//
+//  FrontVideoCapture.swift
+//  Catalyst
+//
+//  Created by Utku Tarhan on 3/31/20.
+//  Copyright © 2020 Utku Tarhan. All rights reserved.
+//
+//  The code was written for personal/educational purposes on San Francisco State University
+//  Does not infringe any conflict of interest with Apple Business Conduct 2020.
+//
+
+
 import UIKit
 import AVFoundation
 import CoreVideo
